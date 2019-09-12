a9c4ec4a86269918eb009197f3919959056acf0e
huyuxia <2316780441@qq.com>
2019-09-12 15:31:23 +0800

    提交最新代码

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ const CompanyService = require("../service/company");
 router.get('/', async (ctx, next) => {
   let banner = await BannerService.getList();
   let company = await CompanyService.getList();
-  console.log(JSON.stringify(company),'company')
+  console.log(JSON.stringify(company), 'company')
   const province = require("../public/javascripts/city");
   await ctx.render('index', {
     title: [
@@ -28,22 +28,36 @@ router.get('/', async (ctx, next) => {
       }
     ],
     list: banner.list,
-    province:JSON.stringify(province),
+    province: JSON.stringify(province),
   })
 })
 
 router.get('company', async (ctx, next) => {
-  await ctx.render('company')
+  let data = await CompanyService.getList();
+  data.list.map(v => {
+    if (v.imgs) {
+      v.imgs = v.imgs.split(',')
+    }
+  })
+  await ctx.render('company', {
+    company: data.list
+  })
 })
 router.get('design', async (ctx, next) => {
-  await ctx.render('design')
+  await ctx.render('design', {
+    images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 1, 1, 1, 1]
+  })
 })
 router.get('bbs', async (ctx, next) => {
-  await ctx.render('bbs')
+  await ctx.render('bbs', {
+    list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 1, 1, 1, 1]
+  })
 })
 
 router.get('service', async (ctx, next) => {
-  await ctx.render('company')
+  await ctx.render('service', {
+    list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 1, 1, 1, 1]
+  })
 })
 
 module.exports = router;
